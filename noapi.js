c3b288ce4df01323a540c264105fbfa3d8a04c9c
David Sáez <dsaezslim@gmail.com>
2022-11-13 09:08:05 -0300

    se añade ejs

--- noapi.js
+++ noapi.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import fetch from "node-fetch";
 
+
 const app = express()
 const PORT = 3005
 app.set('view engine', 'ejs')
@@ -9,20 +10,30 @@ app.use(express.static('public'));
 
 app.use(express.json())
 
-app.get('/', async (req, res) =>{
-    res.render('index')
-})
+let total
 
 app.post('/', async (req, res) =>{
     const { body } = req
     //console.log({ body })
     // Falta obtener el valor total y pintarlo por webnopay
     const boleta = { dinero: body.dinero, tipoPago: body.tipoPago }
-    console.log(boleta)
+    //console.log(boleta)
+    const precioFinal = body.dinero + (10 * (body.dinero/100))
+    total = precioFinal
+
     await fetch('http://localhost:3004/boleta', 
     {method: 'POST', body: JSON.stringify(boleta), 
     headers: {'Content-Type': 'application/json'}})
-    res.sendStatus(201)
+    res.render('index',{total: precioFinal})
 })
+
+app.get('/', async (req, res) =>{
+    res.render('index',{total: total})
+})
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`Escuchando en http://localhost/${PORT}/`))
