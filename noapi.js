41c7dba2197720e3cc057594d971d94b04cf7f13
David Sáez <dsaezslim@gmail.com>
2022-11-13 07:03:57 -0300

    falta pintar el total

--- noapi.js
+++ noapi.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fetch from "node-fetch";
 
 const app = express()
 const PORT = 3005
@@ -6,10 +7,22 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'));
 
+app.use(express.json())
 
-app.get('/', (req, res) =>{
-    //console.log(__dirname)
+app.get('/', async (req, res) =>{
     res.render('index')
+})
+
+app.post('/', async (req, res) =>{
+    const { body } = req
+    //console.log({ body })
+    // Falta obtener el valor total y pintarlo por webnopay
+    const boleta = { dinero: body.dinero, tipoPago: body.tipoPago }
+    console.log(boleta)
+    await fetch('http://localhost:3004/boleta', 
+    {method: 'POST', body: JSON.stringify(boleta), 
+    headers: {'Content-Type': 'application/json'}})
+    res.sendStatus(201)
 })
 
 app.listen(PORT, () => console.log(`Escuchando en http://localhost/${PORT}/`))
